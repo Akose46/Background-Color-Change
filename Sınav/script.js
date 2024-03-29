c63b0705f033813528eafb0908dc76a147995b4e
Akose46 <136177397+Akose46@users.noreply.github.com>
2024-03-29 12:38:48 +0300

    Add files via upload

---
+++ Sınav/script.js
@@ -0,0 +1,4 @@
+document.body.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+function change(){
+    document.body.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+}
